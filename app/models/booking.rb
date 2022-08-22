baaa6e0e5c87025daed99126bf45ca7f43f0efab
Kang Hsieh <kanghsieh1990@gmail.com>
2022-08-22 22:02:51 +0200

    added validations to models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :planet
   belongs_to :spaceship
+
+  validates :user, presence: true
+  validates :spaceship, presence: true
+  validates :planet, presence: true
 end
