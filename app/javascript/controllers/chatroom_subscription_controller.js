53ed92a810525299bfaf13a8420222f33b525c48
Kang Hsieh <kanghsieh1990@gmail.com>
2022-09-02 14:12:51 +0200

    new message triggers form reset and scrolling down to latest message

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -7,6 +7,29 @@ export default class extends Controller {
   static targets = ["messages"]
 
   connect() {
+    this.channel = createConsumer().subscriptions.create(
+      {
+        channel: "ChatroomChannel",
+        id: this.chatroomIdValue,
+      },
+      {
+        received: data => this.#insertMessageAndScrollDown(data)
+      },
+    )
     console.log(`subscribed to the chatroom with the ID ${this.chatroomIdValue}`)
+  }
+
+  disconnect() {
+    console.log("unsubscribed from the chatroom")
+    this.channel.unsubscribe()
+  }
+
+  resetForm(event) {
+    event.target.reset()
+  }
+
+  #insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
 }
